75a96d9d64ae53fb7e2c12b8d2173888f76eb825
Olakunle <olakunle.ibrahim0001@stud.hkr.se>
2021-02-20 16:22:51 +0100

    Census Backend Project

---
+++ src/main/java/com/example/censusAppBackend/CensusAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.censusAppBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CensusAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CensusAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/censusAppBackend/CensusAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.censusAppBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CensusAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
